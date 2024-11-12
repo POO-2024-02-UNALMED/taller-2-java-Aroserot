cf2eaa9735c0b127baf79ac893c8098049b7d275
Aroserot <aroserot@unal.edu.co>
2024-11-12 05:59:22 -0500

    Copiar todo a test

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+
+public class Asiento {
+    String color;
+    int precio;
+    int registro;
+    String[] coloresPosibles = {"rojo", "verde", "amarillo", "negro", "blanco"};
+
+    public void cambiarColor(String color){
+        if (Arrays.asList(coloresPosibles).contains(color)){
+            this.color = color;
+        }
+    }
+}
